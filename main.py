4c164c4904a701132afb87f10cb0f223d9a77648
akshatsri13 <srivastavaakshat022@gmail.com>
2025-10-06 22:10:23 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+class Tea(BaseModel):
+    id: int
+    name: str
+    origin: str
+
+teas: List[Tea] = [] 
+
+@app.get("/")
+def read_root():
+    return {"message" : "Hello from Mobcoder!"}
+
+
+@app.get("/teas/")
+def get_tea():
+    return teas
+
+@app.post("/teas/")
+def add_tea(tea : Tea):
+    teas.append(tea)
+    return tea
+
+
+app.put("/teas/{tea_id}/")
+def update_tea(tea_id: int, updated_tea : Tea):
+    for index,tea in enumerate(teas):
+        if tea.id == tea_id: 
+            tea[index] = updated_tea
+            return updated_tea
+    return {"error": "Tea not found"}    
+
+app.delete("/teas/{tea_id}/")
+def deleted_tea(tea_id: int):
+    for index,tea in enumerate(teas):
+        if tea_id == tea_id:
+            deleted = teas.pop(index)
+            return deleted
+    return {"error": "Tea not found"}        
+
+
